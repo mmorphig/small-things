76233bb2e934e5915ab80151b9c9cbdf68cb16ba
mmorphig <mmorphig@proton.me>
2025-04-19 08:50:44 -0400

    Added a hyperoperation function to the math lib thing.

--- lib/complexmath.c
+++ lib/complexmath.c
@@ -1,20 +1,42 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "complexmath.h"
 
 // This doesn't all have to be 'complex'
-/* As the names imply, setPoint sets the x and y of the point to the inputs
- * changePoint add the new values to the x and y of the point
+/* As the names imply, setVec2 sets the x and y of the Vec2 to the inputs
+ * changeVec2 add the new values to the x and y of the Vec2
  */
-void setPoint(Point *point, int x, int y) {
-	point->x = x;
-	point->y = y;
+void setVec2(Vec2 *Vec2, int x, int y) {
+	Vec2->x = x;
+	Vec2->y = y;
 }
 
-void changePoint(Point *point, int x, int y) {
-	point->x += x;
-	point->y += y;
+void changeVec2(Vec2 *Vec2, int x, int y) {
+	Vec2->x += x;
+	Vec2->y += y;
+}
+
+uint64_t hyperoperation(int n, uint64_t a, uint64_t b, int depth) {
+    if (depth > MAX_HYPEROPERATION_DEPTH) {
+        printf("Exceeded maximum recursion depth!\n");
+        return 0;
+    }
+
+    if (n == 0) return b + 1;
+    if (n == 1) return a + b;
+    if (n == 2) return a * b;
+    if (n == 3) {
+        uint64_t result = 1;
+        for (uint64_t i = 0; i < b; i++) result *= a;
+        return result;
+    }
+
+    // For higher-order operations: H(n, a, b) = H(n - 1, a, H(n, a, b - 1))
+    if (b == 0) return (n == 4) ? 1 : 0;
+
+    return hyperoperation(n - 1, a, hyperoperation(n, a, b - 1, depth + 1), depth + 1);
 }
 
 // Vector math
@@ -23,7 +45,7 @@ Vector* combineVectors(Vector *vect1, Vector *vect2) {
 
 	if (vect1 == NULL || vect2 == NULL) {
 		// If either vectors are null, show the error
-		printf("NULL pointer found in vector combination\n");
+		printf("NULL Vec2er found in vector combination\n");
 		return NULL;
 	}
 
@@ -78,13 +100,13 @@ DecomposedVector decomposeVector(Vector* vect) {
 
 // Misc
 
-// Calculate new points for rotation of a quadrilaterial
-// corners[] is an array of pointers and MUST be used as such (compiler might not like otherwise)
-Point* rotateQuad(Point *corners[], float *degrees) {
+// Calculate new Vec2s for rotation of a quadrilaterial
+// corners[] is an array of Vec2ers and MUST be used as such (compiler might not like otherwise)
+Vec2* rotateQuad(Vec2 *corners[], float *degrees) {
 
 	if (corners[0] == NULL || corners[1] == NULL || corners[2] == NULL || corners[3] == NULL || degrees == NULL) {
 		// If any value is null, show the error
-		printf("NULL pointer found in quadrilaterial rotation");
+		printf("NULL Vec2er found in quadrilaterial rotation");
 		return NULL;
 	}
 
@@ -95,14 +117,14 @@ Point* rotateQuad(Point *corners[], float *degrees) {
 	float centerX = (corners[0]->x + corners[1]->x + corners[2]->x + corners[3]->x) / 4;
 	float centerY = (corners[0]->y + corners[1]->y + corners[2]->y + corners[3]->y) / 4;
 
-	// Function to rotate a point around the center of the box
-	void rotatePoint(Point *p, Point *result) {
+	// Function to rotate a Vec2 around the center of the box
+	void rotateVec2(Vec2 *p, Vec2 *result) {
 		result->x = (float)(cos(radians) * (p->x - centerX) - sin(radians) * (p->y - centerY) + centerX);
 		result->y = (float)(sin(radians) * (p->x - centerX) + cos(radians) * (p->y - centerY) + centerY);
 	}
 
-	// Allocate memory for the new points
-	Point* newCorners = malloc(4 * sizeof(Point));
+	// Allocate memory for the new Vec2s
+	Vec2* newCorners = malloc(4 * sizeof(Vec2));
 	if (newCorners == NULL) {
 		// Memory allocation failed
 		printf("Memory allocation failed in quadrilaterial rotation\n");
@@ -110,10 +132,10 @@ Point* rotateQuad(Point *corners[], float *degrees) {
 	}
 
 	// Rotate all the corners of the box and store them in newCorners
-	rotatePoint(corners[0], &newCorners[0]);
-	rotatePoint(corners[1], &newCorners[1]);
-	rotatePoint(corners[2], &newCorners[2]);
-	rotatePoint(corners[3], &newCorners[3]);
+	rotateVec2(corners[0], &newCorners[0]);
+	rotateVec2(corners[1], &newCorners[1]);
+	rotateVec2(corners[2], &newCorners[2]);
+	rotateVec2(corners[3], &newCorners[3]);
 
 	return newCorners; // Caller must free this memory
 }
@@ -122,7 +144,7 @@ Point* rotateQuad(Point *corners[], float *degrees) {
 
 // Unit circle
 // Probably useless
-Point PointFromAngle(int angle) {
-	Point result = {.x = cos(angle), .y = sin(angle)};  
+Vec2 Vec2FromAngle(int angle) {
+	Vec2 result = {.x = cos(angle), .y = sin(angle)};  
 	return result;
 }
--- lib/complexmath.h
+++ lib/complexmath.h
@@ -1,11 +1,9 @@
-#ifndef VECTOR_MATH_H
-#define VECTOR_MATH_H
+#ifndef COMPLEX_MATH_H
+#define COMPLEX_MATH_H
 
 /* IMPORTANT
  * When using this, make sure every returned value is freed up from ram 
  *   after it is no longer useful.
- * I made this with SDL2 in mind, but have made it completely independent
- *   from it so it can be used in things that don't use SDL2
  * 
  * All units used are assumd to be SI, for accuracy. 
  * Also, everything that would, only has x and y, no z, w, etc
@@ -16,6 +14,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
+
+#define MAX_HYPEROPERATION_DEPTH 100000
 
 typedef struct {
 	// direction is 0 to 360 in degrees
@@ -28,22 +29,38 @@ typedef struct {
 } DecomposedVector;
 
 typedef struct {
-	// x and y could be as int, but for mathematical precision, it's better to use float (or maybe double, but I'm not doing that)
 	float x, y;
-} Point;
+} Vec2;
+
+typedef struct {
+	float x, y, z;
+} Vec3;
+
+typedef struct {
+	float x, y, z, w;
+} Vec4;
 
 typedef struct {
 	float accelX, accelY; // Acceleration of object in both directions
 	float velocityX, velocityY; // Velocity of object in both directions, assumed m/s
 	float mass; // Mass of object, assumed kg
 	float friction; // Friction coeffecient, or 'μ' (just a number, no unit)
-	bool immobile;
+	bool immobile; 
 } ObjectPhysics;
 
-void setPoint(Point *point, int x, int y);
-void changePoint(Point *point, int x, int y);
+// general math functions
+
+uint64_t hyperoperation(int n, uint64_t a, uint64_t b, int depth);
+
+void setVec2(Vec2 *Vec2, int x, int y);
+void changeVec2(Vec2 *Vec2, int x, int y);
+
+// more specific geometric stuff
 
 Vector* combineVectors(Vector *vect1, Vector *vect2);
-Point* rotateBox(Point *corners[], float *degrees);
+Vec2* rotateBox(Vec2 *corners[], float *degrees);
+
+Vec2* rotateQuad(Vec2 *corners[], float *degrees);
+Vec2 Vec2FromAngle(int angle);
 
 #endif
